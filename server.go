43e233523ac2d6e6dc0160f766aa56b619a0b30f
Cilag <guillaume.ozoux@ynov.com>
2022-11-14 15:14:47 +0100

    code server

--- server.go
+++ server.go
@@ -3,21 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"text/template"
 )
 
 const port = ":8080"
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "xHello word!")
+func Index(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "")
 }
 
-func About(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "A propos de moi:")
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	t, err := template.ParseFiles("./templates/" + tmpl + "index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	t.Execute(w, nil)
 }
 
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", Index)
+	http.Handle("/statics/", http.StripPrefix("/statics/", http.FileServer(http.Dir("statics"))))
 	fmt.Println("(http://localhost:8080/) - Server started on port", port)
 	http.ListenAndServe(port, nil)
 }
