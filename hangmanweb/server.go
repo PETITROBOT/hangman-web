0a92e650b3b82f417829f11aa1e07668eafcd788
Cilag <guillaume.ozoux@ynov.com>
2022-11-15 17:17:15 +0100

    code server

--- hangmanweb/server.go
+++ hangmanweb/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	hangmanweb "hangmanweb/utils"
 	"net/http"
 	"text/template"
 )
@@ -10,6 +11,10 @@ const port = ":8080"
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "")
+	l := r.FormValue("name")
+	if l != "" {
+		hangmanweb.HangMan(data, l)
+	}
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string) {
@@ -25,5 +30,8 @@ func main() {
 	http.HandleFunc("/", Index)
 	http.Handle("/statics/", http.StripPrefix("/statics/", http.FileServer(http.Dir("statics"))))
 	fmt.Println("(http://localhost:8080/) - Server started on port", port)
-	http.ListenAndServe(port, nil)
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		fmt.Println("[ERROR] - Server could not start properly.\n ", err)
+	}
 }
